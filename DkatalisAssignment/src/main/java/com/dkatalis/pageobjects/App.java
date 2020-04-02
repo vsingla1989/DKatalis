1033a9670977fd05aa527ad09b8fb845bd7e39e1
Singla, Vikram <vikram.singla@Kronos.com>
2020-04-02 15:59:50 +0530

    Building project structure

---
+++ DkatalisAssignment/src/main/java/com/dkatalis/pageobjects/App.java
@@ -0,0 +1,13 @@
+package com.dkatalis.pageobjects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
